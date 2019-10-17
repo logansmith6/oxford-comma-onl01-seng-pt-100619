6c5020840978eda4c0ea0fb4670181a8f4c8f318
Logan Smith <logsmith06@gmail.com>
2019-10-17 20:08:42 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,9 +5,10 @@ if array.size == 1
 elsif array.size == 2
   array.join(" and ")
 elsif array.size == 3
-  array.join(", ")
-  array.insert(2, "and")
+  new_array = []
 
+    new_array << "#{array[0]}, #{array[1]}, and #{array[2]}"
+    new_array
 end
 
 
