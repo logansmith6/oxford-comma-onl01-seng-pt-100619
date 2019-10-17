2567c1aee33fb50621256619c85d9b5080154679
Logan Smith <logsmith06@gmail.com>
2019-10-17 20:18:53 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,8 +7,9 @@ elsif array.size == 2
 elsif array.size == 3
   array[2] = "and #{array[2]}"
   array.join(", ")
-
+elsif array.size >= 3
+  length = array.size - 1
+  array[length] = "and #{array[length]}"
+  array.join(", ")
 end
-
-
 end
