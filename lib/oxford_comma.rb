34189541c9558ccb17b98049a65be4f74ffa3ceb
Logan Smith <logsmith06@gmail.com>
2019-10-17 20:19:07 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
 
+if array.size == 1
+  return array.join
+elsif array.size == 2
+  array.join(" and ")
+elsif array.size == 3
+  array[2] = "and #{array[2]}"
+  array.join(", ")
+elsif array.size >= 3
+  length = array.size - 1
+  array[length] = "and #{array[length]}"
+  array.join(", ")
+end
 end
